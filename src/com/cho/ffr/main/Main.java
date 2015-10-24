5c3b8632c08a52da06d8546f02a063eb473ea1a4
Frederic DAVIAUD <frederic.daviaud@outlook.com>
2015-10-24 21:11:09 +0200

    Main window

--- src/com/cho/ffr/main/Main.java
+++ src/com/cho/ffr/main/Main.java
@@ -1,9 +1,109 @@
 package com.cho.ffr.main;
 
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		
-	}
+    private static final int WINDOW_HEIGHT = 720;
+    private static final int WINDOW_WIDTH= 1280;
+    private GLFWErrorCallback errorCallback;
+    private GLFWKeyCallback keyCallback;
+
+    private long window;
+
+    public void run() {
+        try {
+            init();
+            loop();
+
+            // Release window and window callbacks
+            glfwDestroyWindow(window);
+            keyCallback.release();
+        } finally {
+            glfwTerminate();
+            errorCallback.release();
+        }
+    }
+
+    private void init() {
+        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (glfwInit() != GL11.GL_TRUE) {
+            throw new IllegalStateException("Unable to initialize GLFW !");
+        }
+        // Configure our window
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+        // Create the window
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "FFR", NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        // Setup a key callback. It will be called every time a key is pressed,
+        // repeated or released.
+        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                    glfwSetWindowShouldClose(window, GL_TRUE);
+                }
+            }
+        });
+
+        GLFWVidMode monitorResolution = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(window, (monitorResolution.getWidth() - WINDOW_WIDTH) / 2, (monitorResolution.getHeight() - WINDOW_HEIGHT) / 2);
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        while (glfwWindowShouldClose(window) == GL_FALSE) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
 }
